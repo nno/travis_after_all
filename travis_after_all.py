ca94387c33b77dee82530c11182b5317841805b3
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2016-07-06 16:19:46 +0200

    ENH: use __main__

--- travis_after_all.py
+++ travis_after_all.py
@@ -1,8 +1,40 @@
+#!/usr/bin/env python
+#
+# travis_after_all.py
+#
+# Retrieved from https://github.com/dmakhno/travis_after_all
+#
+# The main goal of this script to have a single publish when a build has
+# several jobs. Currently the first job is a leader, meaning a node that will
+# do the publishing.
+#
+#    The MIT License (MIT)
+#
+#    Copyright (c) 2014 Dmytro Makhno
+#
+#    Permission is hereby granted, free of charge, to any person obtaining a copy of
+#    this software and associated documentation files (the "Software"), to deal in
+#    the Software without restriction, including without limitation the rights to
+#    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+#    the Software, and to permit persons to whom the Software is furnished to do so,
+#    subject to the following conditions:
+#
+#    The above copyright notice and this permission notice shall be included in all
+#    copies or substantial portions of the Software.
+#
+#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+#    FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+#    COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+#    IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+#    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 import os
 import sys
 import json
 import time
 import logging
+import argparse
 
 try:
     from functools import reduce
@@ -14,43 +46,6 @@ try:
 except ImportError:
     import urllib2
 
-log = logging.getLogger("travis.leader")
-log.addHandler(logging.StreamHandler())
-log.setLevel(logging.INFO)
-
-TRAVIS_JOB_NUMBER = 'TRAVIS_JOB_NUMBER'
-TRAVIS_BUILD_ID = 'TRAVIS_BUILD_ID'
-POLLING_INTERVAL = 'LEADER_POLLING_INTERVAL'
-GITHUB_TOKEN = 'GITHUB_TOKEN'
-
-
-# Travis API entry point, there are at least https://api.travis-ci.com and https://api.travis-ci.org
-travis_entry = sys.argv[1] if len(sys.argv) > 1 else 'https://api.travis-ci.org'
-
-build_id = os.getenv(TRAVIS_BUILD_ID)
-polling_interval = int(os.getenv(POLLING_INTERVAL, '5'))
-gh_token = os.getenv(GITHUB_TOKEN)
-
-# assume, first job is the leader
-
-
-def is_leader(job_number):
-    return job_number.endswith('.1')
-
-job_number = os.getenv(TRAVIS_JOB_NUMBER)
-
-if not job_number:
-    # seems even for builds with only one job, this won't get here
-    log.fatal("Don't use defining leader for build without matrix")
-    exit(1)
-elif is_leader(job_number):
-    log.info("This is a leader")
-else:
-    # since python is subprocess, env variables are exported back via file
-    with open(".to_export_back", "w") as export_var:
-        export_var.write("BUILD_MINION=YES")
-    log.info("This is a minion")
-    exit(0)
 
 
 class MatrixElement(object):
@@ -59,10 +54,9 @@ class MatrixElement(object):
         self.is_finished = json_raw['finished_at'] is not None
         self.is_succeeded = json_raw['result'] == 0
         self.number = json_raw['number']
-        self.is_leader = is_leader(self.number)
 
 
-def matrix_snapshot(travis_token):
+def matrix_snapshot(travis_token, leader_job_number):
     """
     :return: Matrix List
     """
@@ -70,7 +64,8 @@ def matrix_snapshot(travis_token):
     req = urllib2.Request("{0}/builds/{1}".format(travis_entry, build_id), headers=headers)
     response = urllib2.urlopen(req).read()
     raw_json = json.loads(response.decode('utf-8'))
-    matrix_without_leader = [MatrixElement(job) for job in raw_json["matrix"] if not is_leader(job['number'])]
+    matrix_without_leader = [MatrixElement(job) for job in raw_json["matrix"]
+                                    if not is_leader(leader_job_number, job['number'])]
     return matrix_without_leader
 
 
@@ -82,9 +77,9 @@ def wait_others_to_finish(travis_token):
         :return: tuple(True or False, List of not finished jobs)
         """
         snapshot = matrix_snapshot(travis_token)
-        finished = [job.is_finished for job in snapshot if not job.is_leader]
-        return reduce(lambda a, b: a and b, finished), [job.number for job in snapshot if
-                                                        not job.is_leader and not job.is_finished]
+        finished = [job.is_finished for job in snapshot]
+        return all(finished), [job.number for job in snapshot
+                                        if not job.is_finished]
 
     while True:
         finished, waiting_list = others_finished()
@@ -94,26 +89,80 @@ def wait_others_to_finish(travis_token):
         time.sleep(polling_interval)
 
 
-def get_token():
+def get_token(travis_entry, gh_token):
     assert gh_token, 'GITHUB_TOKEN is not set'
     data = {"github_token": gh_token}
     headers = {'content-type': 'application/json'}
 
-    req = urllib2.Request("{0}/auth/github".format(travis_entry), json.dumps(data).encode('utf-8'), headers)
+    req = urllib2.Request("{0}/auth/github".format(travis_entry),
+                            json.dumps(data).encode('utf-8'), headers)
     response = urllib2.urlopen(req).read()
     travis_token = json.loads(response.decode('utf-8')).get('access_token')
 
     return travis_token
 
+def get_argument_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--travis_entry',
+                        default='https://api.travis-ci.org')
+    parser.add_argument('--is_master',action="store_true")
 
-try:
-    token = get_token()
-    wait_others_to_finish(token)
 
-    final_snapshot = matrix_snapshot(token)
-    log.info("Final Results: {0}".format([(e.number, e.is_succeeded) for e in final_snapshot]))
+def current_job_is_leader(master_index):
+    job_number=os.getenv(TRAVIS_JOB_NUMBER)
+    return is_leader(master_index, job_number)
 
+
+def is_leader(master_index, job_number):
+     return job_number.endswith('.%s' % master_index)
+
+def get_job_number():
+    return os.getenv(TRAVIS_JOB_NUMBER)
+
+if __name__=='__main__':
+    log = logging.getLogger("travis.leader")
+    log.addHandler(logging.StreamHandler())
+    log.setLevel(logging.INFO)
+
+
+    TRAVIS_JOB_NUMBER = 'TRAVIS_JOB_NUMBER'
+    TRAVIS_BUILD_ID = 'TRAVIS_BUILD_ID'
+    POLLING_INTERVAL = 'LEADER_POLLING_INTERVAL'
+    GITHUB_TOKEN = 'GITHUB_TOKEN'
     BUILD_AGGREGATE_STATUS = 'BUILD_AGGREGATE_STATUS'
+
+
+    build_id = os.getenv(TRAVIS_BUILD_ID)
+    polling_interval = int(os.getenv(POLLING_INTERVAL, '5'))
+    gh_token = os.getenv(GITHUB_TOKEN)
+    job_number = os.getenv(TRAVIS_JOB_NUMBER)
+
+
+    parser=get_argument_parser()
+    args=parser.parse_args()
+
+    travis_entry = args.travis_entry
+
+    if job_number is None:
+        # seems even for builds with only one job, this won't get here
+        log.fatal("Don't use defining leader for build without matrix")
+        exit(1)
+    elif not is_master:
+        # since python is subprocess, env variables are exported back via file
+        log.info("This is a minion")
+        output_dict=dict(BUILD_MINION="YES")
+        report(output_dict)
+        exit(0)
+
+    log.info("This is a leader")
+    travis_token = get_token(travis_entry, gh_token)
+    wait_others_to_finish(travis_token)
+
+    leader_job_number=get_job_number()
+    final_snapshot = matrix_snapshot(travis_token, leader_job_number)
+    log.info("Final Results: {0}".format([(e.number, e.is_succeeded)
+                                          for e in final_snapshot]))
+
     others_snapshot = [el for el in final_snapshot if not el.is_leader]
     if reduce(lambda a, b: a and b, [e.is_succeeded for e in others_snapshot]):
         os.environ[BUILD_AGGREGATE_STATUS] = "others_succeeded"
